82afdec170b90082b1b4559ca331ab1de2bb5d17
MedhiCallemeyn <callemeynmedhihub@gmail.com>
2023-05-20 15:58:56 +0200

    Update test_utils.py

--- app/test/test_utils.py
+++ app/test/test_utils.py
@@ -20,3 +20,15 @@ def test_discount_price_55_130():
         utils.discount_price(price,discount)
 
 
+def test_discount_price_32e52_55():
+    # Arrange 
+    price = '32.52€'
+    discount = 55
+    #Act   ---->   Assert
+    assert utils.discount_price(price,discount) == '17.88€'
+
+def test_discount_prices_13e66_52e16_30():
+    prices = ['13.66€','52.16€']
+    discount = 30
+
+    assert utils.discount_prices(prices,discount) == ['4.09€','15.64€']
